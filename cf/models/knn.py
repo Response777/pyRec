9e1e20ee892ae1e728ed1e017a9e135151523475
Response777 <CoolResponse777@gmail.com>
2020-07-26 16:56:12 +0200

    Optimize prediction speed.

--- cf/models/knn.py
+++ cf/models/knn.py
@@ -3,16 +3,10 @@ import numba as nb
 from ..utils.transform import sparse_to_dense
 
 
-def predict(dense, sim, ind, row, col, K):
-    knn = []
-    for idx in ind[col]:
-        if len(knn) < K and not np.isnan(dense[row, idx]):
-            knn.append(idx)
-    return (sim[col][knn] * dense[row, knn]).sum() / np.abs(sim[col][knn]).sum()
-
-def sim_matrix(dense):
+def cosine_sim(dense):
+    dense = np.nan_to_num(dense)
     sim_mat = np.dot(dense.T, dense)
-    sum_mat = np.square(dense.T).sum(axis=1)
+    sum_mat = np.sqrt(np.square(dense.T).sum(axis=1))
     return sim_mat / (np.dot(sum_mat[:, np.newaxis], sum_mat[np.newaxis, :]))
 
 class KNN:
@@ -23,20 +17,24 @@ class KNN:
 
     def fit(self, dev_set, val_set, _):
         self.dense = sparse_to_dense(dev_set, self.num_user, self.num_item)
-        self.sim = sim_matrix(np.nan_to_num(self.dense))
+        self.sim = cosine_sim(self.dense)
         self.ind = np.argsort(-self.sim, axis=1)
         print(f"dev err: {self.validate(dev_set)}, val err: {self.validate(val_set)}")
 
     def validate(self, dataset):
         tot = 0.
         for (_, i, j, r) in dataset.itertuples():
-            err = (r - predict(self.dense, self.sim, self.ind, i, j, self.K))
+            err = (r - self.predict(i, j, self.K))
             tot += (err * err)
         return np.sqrt(tot / len(dataset))
       
-    def predict(self, row, col):
-        knn = list(filter(lambda x: not np.isnan(self.dense[row, x]), self.ind[col]))
-        if len(knn) > self.K:
-            knn = knn[:self.K]
-        return (self.sim[col][knn] * self.dense[row][knn]).sum() / np.abs(self.sim[col][knn]).sum()
-       
+    def predict(self, row, col, K = None):
+        rating = self.dense[row, self.ind[col]]
+        weight = self.sim[col][self.ind[col]]
+        weight = weight[~np.isnan(rating)]
+        rating = rating[~np.isnan(rating)]
+        if K != None and len(weight) > K:
+            weight = weight[:K]
+            rating = rating[:K]
+        return (weight * rating).sum() / np.abs(weight).sum()
+      
