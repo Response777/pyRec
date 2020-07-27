88560831f47e2bc500b408b2254799ed4c1be4fb
Response777 <CoolResponse777@gmail.com>
2020-07-27 21:35:22 +0200

    Update knn with different sim metrics

--- cf/models/baseline.py
+++ cf/models/baseline.py
@@ -2,6 +2,7 @@ from ..utils.transform import sparse_to_dense
 
 import numpy as np
 import numba as nb
+import pandas as pd
 
 
 @nb.jit()
@@ -9,31 +10,54 @@ def step(err, b_u, b_i, i, j, lr, reg_b):
     b_u[i] += lr * (err - reg_b * b_u[i])
     b_i[j] += lr * (err - reg_b * b_i[j])
 
-
 class BaselinePredictor:
     def __init__(self, conf):
-        self.b_i = np.random.rand(conf["num_item"])
         self.b_u = np.random.rand(conf["num_user"])
+        self.b_i = np.random.rand(conf["num_item"])
 
         self.reg_b = conf["reg_b"]
         self.lr = conf["lr"]
 
-    def fit(self, dev_set, val_set, num_epoch):
-        X_dev = sparse_to_dense(dev_set, len(self.b_u), len(self.b_i))
+    def build_val_callback(self, val_set):
+        if val_set is None:
+            return lambda *args: None
         X_val = sparse_to_dense(val_set, len(self.b_u), len(self.b_i))
-        dev_mask = ~np.isnan(X_dev)
         val_mask = ~np.isnan(X_val)
+        def score(predict):
+            return np.sqrt(np.square(X_val - predict)[val_mask].sum() / val_mask.sum())
+        return score
+
+    def build_iterator(self, dataset):
+        if isinstance(dataset, pd.DataFrame):
+            def df_iter():
+                for (_, i, j, r) in dataset.itertuples():
+                    yield (i, j, r)
+            return df_iter
+        elif isinstance(dataset, np.ndarray):
+            indices = np.where(~np.isnan(dataset))
+            def np_iter():
+                for (i, j) in zip(*indices):
+                    yield (i, j, dataset[i, j])
+            return np_iter
+        else:
+            raise NotImplementedError
+
+    def fit(self, dev_set, val_set, num_epoch):
+        val_callback = self.build_val_callback(val_set)
+        iterator = self.build_iterator(dev_set)
         for epoch in range(num_epoch):
             tot_dev = 0
-            for (_, i, j, r) in dev_set.itertuples():
+            cnt = 0
+            for (i, j, r) in iterator():
                 err = (r - self.predict(i, j))
                 tot_dev += err * err
+                cnt += 1
                 step(err, self.b_u, self.b_i, i, j, self.lr, self.reg_b)
 
             self.lr = max(self.lr*0.97, 1e-4)
             predict = self.predict_matrix()
-            tot_dev = np.sqrt(np.square(X_dev - predict)[dev_mask].sum() / dev_mask.sum())
-            tot_val = np.sqrt(np.square(X_val - predict)[val_mask].sum() / val_mask.sum())
+            tot_dev = np.sqrt(tot_dev / cnt)
+            tot_val = val_callback(predict)
             print(f"Epoch {epoch}: dev = {tot_dev}, val = {tot_val}")
 
     def predict(self, row, col):
--- cf/models/knn.py
+++ cf/models/knn.py
@@ -1,6 +1,9 @@
+from ..utils.transform import sparse_to_dense
+from ..models import BaselinePredictor
+
 import numpy as np
 import numba as nb
-from ..utils.transform import sparse_to_dense
+from scipy.sparse import csr_matrix
 
 
 def cosine_sim(dense):
@@ -9,16 +12,40 @@ def cosine_sim(dense):
     sum_mat = np.sqrt(np.square(dense.T).sum(axis=1))
     return sim_mat / (np.dot(sum_mat[:, np.newaxis], sum_mat[np.newaxis, :]))
 
+def pearson_sim(dense):
+    return cosine_sim(dense - np.nanmean(dense, axis=1)[:, np.newaxis])
+    
+def baseline_sim(dense):
+    model = BaselinePredictor({
+        "num_user": dense.shape[0],
+        "num_item": dense.shape[1],
+        "reg_b": 0.10,
+        "lr": 0.02
+    })
+    model.fit(dense, None, 3)
+    return cosine_sim(dense - model.predict_matrix())
+
+sim_metrics = {
+    "cosine": cosine_sim,
+    "pearson": pearson_sim,
+    "baseline": baseline_sim,
+}
+
 class KNN:
     def __init__(self, conf):
         self.num_user = conf["num_user"]
         self.num_item = conf["num_item"]
         self.K = conf["K"]
+        self.sim_func = sim_metrics[conf["similarity"]]
 
     def fit(self, dev_set, val_set, _):
         self.dense = sparse_to_dense(dev_set, self.num_user, self.num_item)
-        self.sim = cosine_sim(self.dense)
+        self.sim = self.sim_func(self.dense)
         self.ind = np.argsort(-self.sim, axis=1)
+
+        mask = csr_matrix(~np.isnan(self.dense).T.astype(int))
+        U = (mask * mask.T).toarray()
+        self.sim *= (U - 1) / (U - 1 + 100) 
         print(f"dev err: {self.validate(dev_set)}, val err: {self.validate(val_set)}")
 
     def validate(self, dataset):
